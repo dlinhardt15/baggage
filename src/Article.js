152087bc169632c275b96345863dfe1961adda6b
Danny Linhardt <daniel.linhardt@gmail.com>
2022-11-18 16:54:06 -0500

    fixed patch

--- src/Article.js
+++ src/Article.js
@@ -1,29 +1,45 @@
-export default function Article ({article}) {
+import {useState} from "react";
 
-    function changeCount (event, article) {
-        const count = parseInt(event.target.value)
-        fetch("http://localhost:3001/items/" + article.id, {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({count})
-        })
-        .then(response => response.json())
-        .then(data => console.log(data))
+export default function Article ({article, setSuitcase, suitcase}) {
 
-        // const weightInGrams = event.target.value * article.weight
-        // const weightInPounds = weightInGrams * 0.00220462
-        // const weight = Math.round(weightInPounds * 10)/10
-        // console.log(weight)
-        // setSuitcase(suitcase + weightInPounds)
+    const [count, setCount] = useState(article.count)
+    
+    // Click event behavior dependent on whether adding or removing item from suitcase.
+    function handleClick (event) {
+        event.target.innerText === "Add" ?
+            fetch("http://localhost:3001/items/" + article.id, {
+             method: "PATCH",
+                headers: {
+                 "Content-Type": "application/json"
+                },
+                body: JSON.stringify({count: count + 1})
+            })
+            .then(response => response.json())
+            .then(data => {
+                setCount(data.count)
+                setSuitcase(suitcase + data.weight)
+            })
+        :
+            fetch("http://localhost:3001/items/" + article.id, {
+             method: "PATCH",
+                headers: {
+                 "Content-Type": "application/json"
+                },
+                body: JSON.stringify({count: count - 1})
+            })
+            .then(response => response.json())
+            .then(data => {
+                setCount(data.count)
+                setSuitcase(suitcase - data.weight)
+            })
     }
 
 
     return (
         <div>
-            <span>{article.name}:</span>
-            <span><input type="number" min="0" onChange={(e) => changeCount(e, article)}/></span>
+            <span>{article.name}: {count}</span>
+            <span onClick={(event) => handleClick(event)}>Add</span>
+            <span onClick={(event) => handleClick(event)} value="-1">Remove</span>
         </div>
         )
 
--- src/ArticlePage.js
+++ src/ArticlePage.js
@@ -4,25 +4,26 @@ import Article from "./Article";
 export default function ArticlePage () {
     const [articles, setArticles] = useState([])
     const [suitcase, setSuitcase] = useState(0)
-    const [articleWeight, setArticleWeight] = useState(0)
 
+    // Collecting article data from db.json
     useEffect(() => {
         fetch("http://localhost:3001/items")
         .then(response => response.json())
         .then(data => setArticles(data))
     }, [])
 
-    // function to get weight of items.( convert from grams to pounds) add weight to suitcase
-
+    // Converting suitcase weight from grams to pounds
+    const suitcaseInPounds = Math.round((suitcase * 0.00220462) * 10) / 10
     
     return (
         <div>
             {articles.map(article => {
                 return (
-                <Article article={article}/>)
+                <Article article={article} suitcase={suitcase} setSuitcase={setSuitcase}/>)
             })}
-            <div>Your suitcase weighs {suitcase} pounds.</div>
-            {suitcase <= 50 ? <div>Your bag will make it!</div> : <div>You need to make room.</div>}
+            <div>Your suitcase weighs {suitcaseInPounds} pounds.</div>
+            {suitcaseInPounds <= 50 ? <div>Your bag will make it!</div> : <div>You need to make room.</div>}
+            
         </div>
     )
 }
